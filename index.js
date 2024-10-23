a3021bfcd27819f292c6d51ab7cfa60f1869d0c3
Karanrajsinh <vaghelakaranraj54@gmail.com>
2024-10-23 08:56:18 +0530

    Tailwind Init

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ app.post('/generatePdf', async (req, res) => {
         const page = await browser.newPage();
 
         await page.setContent(htmlContent, { waitUntil: 'networkidle0' });
-        await page.addStyleTag({ path: 'https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css' });
 
         // Inject CSS for page size and margins using the received dimensions
         await page.addStyleTag({
